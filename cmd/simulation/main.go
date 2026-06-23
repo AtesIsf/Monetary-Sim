75e2df8d2075210f3242e436b98999a29a715faa
AtesIsf <isfendiyar510@gmail.com>
2026-06-23 21:42:03 +0300

    I think it works, probably has minor bugs remaining

--- cmd/simulation/main.go
+++ cmd/simulation/main.go
@@ -4,11 +4,8 @@ import "github.com/AtesIsf/monetary-simulator/internal/engine"
 
 func main() {
 	var sim engine.Simulation
-	/*
-	In the US, there are approximately 33.2M businesses and 350M people.
-	So, approximately using the ratio 1/10 firms to households should be ok
-	*/
-	sim.Populate(1000, 100)
+
+	sim.Populate(500, 160)
 	defer sim.Close()
 
 	// 4 -> 20 -> 2
